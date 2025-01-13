f5596ffd8a171f8a691e03ce23c161d9b5e77293
Avishekh Sinha (Avi) <39235012+avishekhsinhaRepo@users.noreply.github.com>
2025-01-13 10:55:50 +0530

    Table Added

--- streamlit-app/main.py
+++ streamlit-app/main.py
@@ -2,13 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+df = pd.read_csv("data/quarterly_canada_population.csv",dtype=str)
 
 st.title("Population of Canada")
 
 st.markdown("Source can be found [here](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1710000901)")
 
 with st.expander("See the full table"):
-    st.write("This is the full table")
+    st.dataframe(df)
 
 with st.form("population_form"):
     col1, col2, col3 = st.columns(3)
@@ -25,26 +26,21 @@ with st.form("population_form"):
 
     with col3:
         st.write("Choose the Location")
-        st.selectbox("Quarter", options=['Q1', 'Q2', 'Q3', 'Q4'], index=3, key="location")
+        st.selectbox("Choose the Location", options=['Q1', 'Q2', 'Q3', 'Q4'], index=3, key="location")
 
     st.form_submit_button("Analyze", type="primary")
-
 
 tab1, tab2 = st.tabs(['Population Change', 'Compare'])
 
 with tab1:
-    st.subheader("Population Change")
+    st.subheader(f"Population Change from {start_quarter} {start_year} to {end_quarter} {end_year}")
     col1, col2 = st.columns(2)
 
     with col1:
-        st.write(f"Population Change from {start_quarter} {start_year} to {end_quarter} {end_year}")
-        st.write(f"{start_quarter} {start_year}")
-        st.write("Q3 1991")
-        st.write(f"{end_quarter} {end_year}")
-        st.write("Q3 1991")
+        st.metric(f"{start_quarter} {start_year}", "10,000")
+        st.metric(f"{end_quarter} {end_year}", "10,000", delta=10, delta_color="normal")
     with col2:
         st.write("Population Change col2")
-
 
 with tab2:
     st.subheader("Compare with other locations")
